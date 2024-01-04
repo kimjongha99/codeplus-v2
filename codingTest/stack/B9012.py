d200d4ba536da9abe4e8e8a523204732f65a6604
kimjongha99 <jjhsun99@gmail.com>
2024-01-04 21:00:46 +0900

    Update B9012.py

--- codingTest/stack/B9012.py
+++ codingTest/stack/B9012.py
@@ -1,32 +1,33 @@
 from sys import stdin
 
-
-def solution(str):
-    stack = []
-
-    for s in str:
-        if s == '(':  # 열린 괄호를 만나면 스택에 추가
-            stack.append(s)
-        elif s == ')':  # 닫힌 괄호를 만나면
-            if not stack:  # 스택이 비어있으면 균형이 맞지 않음
-                return "NO"
-            if stack[-1] == '(':  # 스택의 마지막이 열린 괄호면 짝이 맞으므로 pop
-                stack.pop()
-            else:  # 짝이 맞지 않으면
-                return "NO"
-    # 모든 검사 후 스택이 비어있으면 균형이 맞는 것
-    return "YES" if not stack else "NO"
-
-
-
 input = stdin.readline
 
 
+tc = int(input())
 
-n = int(input())
-str = ''
-for i in range(n):
+for _ in range(tc):
     str = input()
-    print(solution(str))
+    stack = []
+    balanced =True  # 괄호가 맞는지
+
+
+    for s in str:
+        if s == '(':
+            stack.append(s)
+        elif s == ')':
+            if not stack:  # 스택이 비어있을때
+                print("NO")
+                balanced = False
+                break
+            if stack[-1]=='(':
+                stack.pop()
+            else:
+                print("NO")
+                balanced=False
+                break
+    if balanced and not stack:  # Check if parentheses are balanced and stack is empty
+        print("YES")
+    elif balanced:
+        print("NO")
 
 
