a60a070443f929766eab71b58a502424bbd9509e
kimjongha99 <jjhsun99@gmail.com>
2024-01-04 18:51:59 +0900

    Update leetcode225.py

--- codingTest/queue/leetcode225.py
+++ codingTest/queue/leetcode225.py
@@ -1,6 +1,7 @@
 import collections
 from collections import deque
 
+
 class MyStack:
 
     def __init__(self):  # Python에서 __init__은 클래스의 새 인스턴스가 생성될 때 자동으로 호출되는 특수 메서드
@@ -9,23 +10,23 @@ class MyStack:
     def push(self, x: int) -> None:
         self.q.append(x)
 
-        for _ in range(len(self.q) - 1):  #_ 이거는 의미없는 변수. q의 제일 뒷 요소 앞에 추가.
+        for _ in range(len(self.q) - 1):  # _ 이거는 의미없는 변수. q의 제일 뒷 요소 앞에 추가.
             self.q.append(self.q.popleft())
 
-
     def pop(self) -> int:
-      return  self.q.popleft()
+        return self.q.popleft()
 
     def top(self) -> int:
         return self.q[0]
 
     def empty(self) -> bool:
-        return  len(self.q)==0  # Simplified check for emptiness
+        return len(self.q) == 0  # Simplified check for emptiness
+
 
 # Example usage
 myStack = MyStack()
 myStack.push(1)
 myStack.push(2)
-print("Top:", myStack.top())    # return 2
-print("Pop:", myStack.pop())    # return 2
-print("Empty:", myStack.empty()) # return False
+print("Top:", myStack.top())  # return 2
+print("Pop:", myStack.pop())  # return 2
+print("Empty:", myStack.empty())  # return False
