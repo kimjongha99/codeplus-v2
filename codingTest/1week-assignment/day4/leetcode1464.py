c182c047cfa2ca98b52edcf9cabcad4d58509079
kimjongha99 <jjhsun99@gmail.com>
2024-01-06 15:43:07 +0900

    Create leetcode1464.py

---
+++ codingTest/1week-assignment/day4/leetcode1464.py
@@ -0,0 +1,29 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        heapq.heapify(nums)  # 힙으로 변환해줘야 함
+        n = len(nums)
+        for _ in range(n-2):
+            heapq.heappop(nums)
+
+        n1= heapq.heappop(nums)
+        n2= heapq.heappop(nums)
+
+        return (n1-1) * (n2-1)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    nums =[10,2,5,2]
+    sol = Solution()
+    result = sol.maxProduct(nums)
+    print("Output:", result)
