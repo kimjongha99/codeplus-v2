dd082dc23053f5c1ea6077bc6817932eb912cd4c
kimjongha99 <jjhsun99@gmail.com>
2024-01-08 16:48:26 +0900

    Create B5397.py

---
+++ codingTest/1week-assignment/day5/B5397.py
@@ -0,0 +1,25 @@
+import sys
+
+tc = int(sys.stdin.readline())
+
+for _ in range(tc):
+    str = input()
+
+    left_list=[]
+    right_list=[]
+
+    for s in str:
+        if s == '>':
+            if right_list:
+                left_list.append(right_list.pop())
+        elif s == '<':
+            if left_list:
+                right_list.append(left_list.pop())
+        elif s == '-':
+            if left_list:
+                left_list.pop()
+        else:
+            left_list.append(s)
+
+    final_output = ''.join(left_list) + ''.join(reversed(right_list))
+    print(final_output)
