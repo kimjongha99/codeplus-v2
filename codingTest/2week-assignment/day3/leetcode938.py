1e48f4997cce04431d69a65cc53a8d866f4a6b66
kimjongha99 <jjhsun99@gmail.com>
2024-01-13 18:22:51 +0900

    .

--- codingTest/2week-assignment/day3/leetcode938.py
+++ codingTest/2week-assignment/day3/leetcode938.py
@@ -1,15 +1,28 @@
-#https://leetcode.com/problems/range-sum-of-bst/description/
-#Range Sum of BST
+#https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+# Search in a Binary Search Tree
+from idlelib.tree import TreeNode
+from typing import Optional
 
 
 class Solution:
-    def rangeSumBST(self, root, L, R):
-        if not root:
-            return 0
 
-        if L <= root.val <= R:
-            return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
-        elif root.val < L:
-            return self.rangeSumBST(root.right, L, R)
-        else:
-            return self.rangeSumBST(root.left, L, R)
+    def __init__(self):
+        self.result = None
+
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.result = 0
+        self.recur(root, low, high)
+        return self.result
+
+    def recur(self, node: TreeNode, low: int, high: int):
+        # 노드가 없다 = 리프 노드의 자식 노드
+        if node is None:
+            return
+
+        # 현재 노드가 문제의 범위에 포함되면 결과값에 더함
+        if low <= node.val <= high:
+            self.result += node.val
+
+        # 현재 노드의 왼쪽, 오른쪽 자식 노드를 재귀적으로 탐색
+        self.recur(node.left, low, high)
+        self.recur(node.right, low, high)
