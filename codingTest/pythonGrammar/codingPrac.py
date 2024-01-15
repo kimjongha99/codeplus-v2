aacf418f319fb6e2dbb94bab1567a6a804141e10
kimjongha99 <jjhsun99@gmail.com>
2024-01-15 22:26:58 +0900

    Create codingPrac.py

---
+++ codingTest/pythonGrammar/codingPrac.py
@@ -0,0 +1,71 @@
+# from collections import Counter
+# from itertools import combinations, product
+#
+# data = dict()
+#
+#
+# data['사과']= 'apple'
+#
+# print(data)
+#
+# #해시
+#
+# b ={
+#     '이순신':'명량',
+#     '강감찬':'장군'
+# }
+#
+# print(b)
+#
+#
+# print(b.keys())
+# print(b.values())
+# print(list,b.keys())
+# value_list = list(b.values())
+# print(value_list)
+#
+# print(b['이순신'])
+#
+# print('\n')
+#
+# #set 은 중복X 순서 없음 리스트 혹은 문자열 을 이용해서 초기화 가능
+#
+#
+# data = set([1,1,2,2,3,3,4,5])
+# print(data)
+#
+# data2={1,1,2,2,3,4,5}
+# print(data2)
+#
+#
+#
+# #5개의 숫자를 입력받고 내림차순
+# # data3 = list(map(int,input().split()))
+# # print(data3)
+# # data3.sort(reverse=True)
+# # print(data3)
+#
+# # #3개의 인풋을 받음
+# # a ,b,c = map(int,input().split())
+# # print(a,b,c)
+#
+#
+#
+#
+# # 조합 순서를 고려하지않고 두개를 뽀는경우
+#
+# data4 =['A','B','C']
+#
+# result = list(combinations(data4,2))
+# print(result)
+#
+# data5 =['B','C','D']
+# result2 =list(product(data5,repeat=2))
+# print(result2)
+# counter = Counter(result2)
+# print(dict(counter))
+
+
+
+
+
